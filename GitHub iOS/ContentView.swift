8d9f65e944c7968b18017032c39b3f1ea0474076
linder3hs <linderhassinger00@gmail.com>
2022-06-29 01:06:50 -0500

    Initial Commit

---
+++ GitHub iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHub iOS
+//
+//  Created by Linder Anderson Hassinger Solano    on 29/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub iOS/GitHub_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_iOSApp.swift
+//  GitHub iOS
+//
+//  Created by Linder Anderson Hassinger Solano    on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
